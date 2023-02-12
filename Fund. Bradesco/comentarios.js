8298255e6ab40c6b9665ae180a07743694f81739
Ellen Almeida <103392127+EllenAlmeida13@users.noreply.github.com>
2023-02-12 14:42:07 -0300

    Add files via upload

---
+++ Fund. Bradesco/comentarios.js
@@ -0,0 +1,77 @@
+// comentário em linha
+
+/* comentário de 
+bloco */
+
+/*
+
+Alt + Z quebra linha
+
+console.log('Bem vindo ao Starter!!')
+
+"" // aspas duplas (usar fora quando for usar a simples dentro)
+'' // aspas simples (usar fora quando for usar as dupls fora)
+`` // template literals ou tamplates strings (permitem usar multilinhas, expressõs de linguagens)
+
+## Primitivos
+
+*Number
+
+33 // inteiros
+12.5 // reais - floate
+NaN // Not a number
+Infinity (I precisa ser maiusculo) // infinito
+
+*Boolean (Booleano)
+
+Somente 2 valores
+
+true // verdadeiro
+false // false
+
+* underfined
+indefinido
+
+*null
+
+nulo
+objeto que não possui nada dentro
+diferente de indefinido
+
+## Estruturais
+
+*Object
+
+objeto
+propriedade /atributos
+funcionalidades/métodos
+
+{propriedade: "value", "value"}
+
+*Array (Vetores)
+
+uma lista
+agrupamento de dados
+
+[2 "Leites", "Ovos"]
+
+## Variáveis
+
+Nomes simbólicos para receber algum valor
+Atalhos de código
+Identificadores
+
+*3 palavras rezervadas para criar uma variável
+
+var
+let
+const (não pode mudar seu valor durante sua a aplicação)
+
+## Scope
+
+*Escopo determina a visibilidade de alguma variavel no JS
+
+# Bloco statement
+0
+{qualquer variável dentro do bloco}
+*/
